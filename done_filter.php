728cd65b78d7e25ca70d7050b9f60872c759148d
Tanishq Parkar <78678620+RevTpark@users.noreply.github.com>
2022-04-25 00:20:05 +0530

    added more functionality

---
+++ done_filter.php
@@ -0,0 +1,24 @@
+<?php
+include 'config.php';
+
+$query = "SELECT * FROM task WHERE done=1";
+$data = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($data) > 0){
+    while($row = mysqli_fetch_assoc($data)){
+?>
+
+<li class="task w-50 p-2 mb-3 <?php if($row['done'] == 1){ echo "done-task"; } else{ echo "undone-task"; }?>">
+    <span class="task-title"><?php echo $row['title']; ?></span>
+    <button class="btn btn-info" id="updateTask" data-id="<?php echo $row['id']; ?>"><i class="fa fa-pencil"></i></button>
+    <button class="btn btn-danger" id="deleteTask" data-id="<?php echo $row['id']; ?>"><i class="fa fa-trash"></i></button>
+</li>
+
+<?php
+    }
+}
+else{
+    echo "No Record Exists";
+}
+
+?>
